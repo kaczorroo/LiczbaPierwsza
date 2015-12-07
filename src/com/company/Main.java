81754592449927892a727c4308bc0fbe38a3ba0f
Daniel Kaczorowski <kaczorowski.daniel@gmail.com>
2015-12-07 11:43:18 +0100

    1st

---
+++ src/com/company/Main.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// Napisaæ program, który sprawdza czy podana liczba n,n>1, jest liczb¹ pierwsz¹.
+    }
+}
